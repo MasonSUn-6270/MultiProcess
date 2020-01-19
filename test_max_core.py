2e0d3661e4a86ea3ce1bf2e2e33e353f435762d7
MasonSun <980619554@qq.com>
2020-01-19 13:44:51 +0800

    sick somehow, i simplified lines again

--- test_max_core.py
+++ test_max_core.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from functools import wraps
 
 
-
 def count_time(func):
     @wraps(func)
     def wrapper(t):
@@ -18,9 +17,8 @@ def count_time(func):
 
 
 @count_time
-def run(core_num):
+def multi_core(core_num):
     [multiprocessing.Process(target=foo).start() for _ in range(core_num)]
-    print(f'active_children_num : {len(multiprocessing.active_children())}')
     [process.join() for process in multiprocessing.active_children()]
 
 
@@ -34,18 +32,22 @@ def normal(time_):
 
 def collect_data(max) -> dict:
     data = {}
-    for func in [run, normal]:
-        data[func.__name__] = {}
-        for i in range(1, max + 1, 5):
-            data[func.__name__][i] = func(i)
+    for func in [multi_core, normal]:
+        data[func.__name__] = []
+        [data[func.__name__].append(func(i)) for i in range(1, max + 1, 5)]
     return data
 
 
-if __name__ == '__main__':
-    data = collect_data(20)
-    [plt.plot(list(data[i].keys()), list(data[i].values()), label='multi') for i in data.keys()]
+def main(num: int):
+    data = collect_data(num)
+    [plt.plot(list(range(1, num + 1, 5)), list(data[i]),ls='-.' ,label=i) for i in data.keys()]
     plt.legend()
+    plt.title('Time to complete unit work')
     plt.xlabel('process NUM')
     plt.ylabel('second spend')
     plt.tight_layout()
     plt.show()
+
+
+if __name__ == '__main__':
+    main(20)
